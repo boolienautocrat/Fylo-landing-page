f68b84537456bde8c73446f10d139931df80336b
Jose <josetsaji2006@gmail.com>
2023-04-25 08:46:55 +0530

    add progress bar & gtag and  update personal website link

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-var button=document.querySelector(".btn-btt"),html=document.querySelector("html"),errorMsg=document.querySelector(".error-msg"),input=document.querySelector("input");window.addEventListener("scroll",function(){html.scrollTop>1113?button.style.display="block":html.scrollTop<1113&&(button.style.display="none")}),button.addEventListener("click",function(){html.scrollTo(0,0)}),window.matchMedia("(max-width: 400px)").matches&&window.addEventListener("scroll",function(){html.scrollTop>689?button.style.display="block":html.scrollTop<689&&(button.style.display="none")}),input.addEventListener("invalid",function(t){t.preventDefault()}),input.addEventListener("keypress",function(){input.checkValidity()?input.checkValidity()&&(errorMsg.innerHTML=" "):errorMsg.innerHTML="Please enter a valid email address"});
+var button = document.querySelector(".btn-btt"), html = document.querySelector("html"), errorMsg = document.querySelector(".error-msg"), input = document.querySelector("input"); window.addEventListener("scroll", function () { html.scrollTop > 1113 ? button.style.display = "block" : html.scrollTop < 1113 && (button.style.display = "none") }), button.addEventListener("click", function () { html.scrollTo(0, 0) }), window.matchMedia("(max-width: 400px)").matches && window.addEventListener("scroll", function () { html.scrollTop > 689 ? button.style.display = "block" : html.scrollTop < 689 && (button.style.display = "none") }), input.addEventListener("invalid", function (t) { t.preventDefault() }), input.addEventListener("keypress", function () { input.checkValidity() ? input.checkValidity() && (errorMsg.innerHTML = " ") : errorMsg.innerHTML = "Please enter a valid email address" });
+
+window.onscroll = function () {myFunction()}
+
+function myFunction() {
+    var winScroll = document.documentElement.scrollTop
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    var scroll = (winScroll/height) * 100
+    document.getElementById("myBar").style.width = scroll + "%"
+}
